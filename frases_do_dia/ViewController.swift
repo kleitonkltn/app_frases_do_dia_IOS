8672e5af29c4b923ea981cbc3168c06079ed57d7
Kleiton De Souza Rocha <kleitonkltn@gmail.com>
2021-04-26 14:45:17 -0400

    Initial Commit

---
+++ frases_do_dia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  frases_do_dia
+//
+//  Created by Mac KR on 26/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
