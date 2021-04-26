27809f8ac6aa016a0c878906068a86a74c3bb41d
Kleiton De Souza Rocha <kleitonkltn@gmail.com>
2021-04-26 15:17:24 -0400

    create storyBoard main and implement function for set random phrases of click in button

--- frases_do_dia/ViewController.swift
+++ frases_do_dia/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelPhrase: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func newPrhase(_ sender: Any) {
+        var frases:[String] = []
+        frases.append("Deixe pra trás o que não te leva pra frente.")
+        frases.append("O segredo é ter fé em Deus.")
+        frases.append("Falo nada, só observo.")
+        frases.append("Obrigado Deus pelo dia de hoje!")
+        frases.append("Insista, persista e nunca desista.")
+        let numero = arc4random_uniform(UInt32(frases.count)-1)
+        labelPhrase.text = frases[Int(numero)]
 
+    }
 }
 
